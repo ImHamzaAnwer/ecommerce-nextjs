f11c21032564d242ee0754e32a4e95ee0e7b20e0
Hamza Anwer <hamza.anwer8@gmail.com>
2023-12-16 15:02:18 +0500

    create dynamic edit page for product

--- ecommerce-admin/pages/api/products.js
+++ ecommerce-admin/pages/api/products.js
@@ -1,12 +1,16 @@
 import { mongooseConnect } from "@/lib/mongoose";
-import { Product } from "@/models/product";
+import { Product } from "@/models/Product";
 
 export default async function handle(req, res) {
   mongooseConnect();
   const { method } = req;
 
   if (method === "GET") {
-    res.json(await Product.find());
+    if (req.query?.id) {
+      res.json(await Product.findOne({ _id: req.query.id }));
+    } else {
+      res.json(await Product.find());
+    }
   }
 
   if (method === "POST") {
